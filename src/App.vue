9a87d14129eb54dcc9bcac55e2039124e8805127
Frank Wei <frank@frankwei.xyz>
2018-10-11 15:18:50 +0800

    Login by Scatter OK

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,83 @@
 <template>
   <div id="app" class="container">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav class="level" style="height: 4rem">
+      <div class="level-left">
+        <router-link to="/" class="level-item has-text-centered">Home</router-link>
+        <router-link to="/about" class="level-item has-text-centered">About</router-link>
+      </div>
+      <div class="level-right" v-if="account !== null">
+            <p class="level-item has-text-centered">
+              {{account.name}}
+            </p>
+            <button class="button" @click="forgetId">退出登录</button>
+      </div>
+    </nav>
+    <div class="please-login" v-if="account === null">
+      <section class="hero is-primary">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              欢迎来到加密名人
+            </h1>
+            <h2 class="subtitle">
+              请使用钱包进行登录以继续
+            </h2>
+            <button class="button" @click="requestId" :disabled="!scatter"> 登录 </button>
+          </div>
+        </div>
+      </section>
     </div>
-    <router-view/>
+    <router-view v-else/>
   </div>
 </template>
+
+<script>
+import { mapState, mapActions, mapGetters } from 'vuex';
+import { network } from './config';
+
+const requiredFields = { accounts: [network] };
+
+export default {
+  name: 'app',
+  data: () => ({}),
+  created() {
+    // @TODO: replace with Scatter JS
+    document.addEventListener('scatterLoaded', () => {
+      console.log('Scatter Loaded');
+      this.handleScatterLoaded();
+    });
+  },
+  methods: {
+    ...mapActions(['initScatter', 'setIdentity']),
+    handleScatterLoaded() {
+      const { scatter } = window;
+      this.initScatter(scatter);
+      // this.requestId();
+    },
+    async requestId() {
+      await this.suggestNetworkSetting();
+      const identity = await this.scatter.getIdentity(requiredFields);
+      this.setIdentity(identity);
+    },
+    async forgetId() {
+      await this.scatter.forgetIdentity();
+      this.setIdentity(null);
+    },
+    async suggestNetworkSetting() {
+      try {
+        await this.scatter.suggestNetwork(network);
+      } catch (error) {
+        console.info('User canceled to suggestNetwork');
+      }
+    },
+  },
+  computed: {
+    ...mapState(['identity', 'scatter', 'eos']),
+    ...mapGetters(['account']),
+  },
+};
+</script>
+
 
 <style>
 #app {
