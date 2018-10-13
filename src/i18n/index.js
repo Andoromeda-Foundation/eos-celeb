13feb933d433dc8cbd779bf3c126194baa4572b4
Frank Wei <frank@frankwei.xyz>
2018-10-13 10:54:50 +0800

    refactor: remove useless code

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,23 +1,8 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import { messages } from './locales'
-// import en from "./locales/en";
-// import { i18n } from '@/config'
 
 Vue.use(VueI18n)
-
-// const messages = {
-//   'zh': zh_CN,
-//   'en': en
-// }
-
-// i18n.forEach((item) => {
-//   // eslint-disable-next-line
-//   const locale = require(`./locales/${item.locale}`);
-//   item.aliases.forEach((alias) => {
-//     messages[alias] = locale
-//   })
-// })
 
 export default new VueI18n({
   locale: 'zh',
